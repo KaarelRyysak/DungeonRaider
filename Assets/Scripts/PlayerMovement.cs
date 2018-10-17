3d0a68fb6a607037c3a0596c2a8977577f0bebf0
K-W S <karl-walter@hotmail.com>
2018-10-17 13:12:25 +0300

    Animation added (jump animation currently has a hack for a longer animation, not a proper one)

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,29 +5,48 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour {
 
     public CharacterController2D Controller;
+    public Animator animator;
 
-    float runSpeed = 40f;
+    float runSpeed = 10f;
     float hMove = 0f;
 
     bool jump = false;
+    //float vMoveOld;
+    //float vMoveNew;
     bool crouch = false;
+
+    //Note: Currently player uses "slippery" material,
+    //which means he won't stick to walls when you run into it.
+    //If you want that, make a new material with some friction
+    //and apply it to him (box and circlecolliders).
 
 
 	// Use this for initialization
 	void Start () {
-		
+        //vMoveNew = transform.position.y;
 	}
 	
 	// Update is called once per frame
     //Get variables
 
 	void Update () {
+        //Get left right movement
+        hMove = Input.GetAxisRaw("Horizontal") * runSpeed; 
+        animator.SetFloat("Speed", Mathf.Abs(hMove));
 
-        hMove = Input.GetAxisRaw("Horizontal") * runSpeed; //Get left right movement
+        //Jump is pressed
         if (Input.GetButtonDown("Jump")) {
             jump = true;
+            animator.SetBool("IsJumping", true);
         }
 
+        //Get vertical movement
+        //vMoveOld = vMoveNew;
+        //vMoveNew = transform.position.y;
+        //animator.SetFloat("HeightSpeed", vMoveNew - vMoveOld);
+
+
+        //Crouch is pressed
         if (Input.GetButtonDown("Crouch"))
         {
             crouch = true;
@@ -39,6 +58,11 @@ public class PlayerMovement : MonoBehaviour {
         
 
 	}
+
+    public void onLanding() {
+        animator.SetBool("IsJumping", false);
+    }
+
     //Apply variables
     private void FixedUpdate()
     {
