bdd511a66b01c90bca0cc4de9dc5ec8119d00088
OliVinDiisel <oliver.vinkel@gmail.com>
2018-11-02 14:28:10 +0200

    started work on spear item, problems with rotating and directional force (coordinate system scales slower in X direction than Y)

--- Assets/Scripts/UI+Items/Spear.cs
+++ Assets/Scripts/UI+Items/Spear.cs
@@ -1,15 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.EventSystems;
 
-public class RuneTeleport : MonoBehaviour, Consumable {
+public class Spear : MonoBehaviour, Consumable {
     SpriteRenderer spriteRenderer;
     public Color normalColor;
     public Color highlightColor;
     private GameObject player;
-    public float interactRange = 1.5f;
-    public float itemSize = 0.2f;
+    public float interactRange = 10f;
+    public float itemSize = 10f;
     public InventoryButton lmb;
     public InventoryButton rmb;
     private MousePointer mousePointer;
@@ -17,12 +16,12 @@ public class RuneTeleport : MonoBehaviour, Consumable {
     //Set by mousePointer, is it close to mouse?
     private bool highlighted;
 
-    
+
     private bool glowing;
     public bool pickedUp;
 
-    // Use this for initialization
-    void Start () {
+    void Start()
+    {
         spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
 
         //Finds the gameobject called "Player" and assigns it
@@ -35,9 +34,9 @@ public class RuneTeleport : MonoBehaviour, Consumable {
         glowing = false;
         pickedUp = false;
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    void Update()
+    {
         //If it's highlighted, for each button, if the button is pressed, try to take it
         if (highlighted && !pickedUp)
         {
@@ -64,7 +63,7 @@ public class RuneTeleport : MonoBehaviour, Consumable {
                 }
             }
         }
-        else if(glowing && !pickedUp)
+        else if (glowing && !pickedUp)
         {
             spriteRenderer.color = normalColor;
             //TODO: implement highlighting here !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
@@ -77,10 +76,29 @@ public class RuneTeleport : MonoBehaviour, Consumable {
     //This is what happens when the item is used (button released). Called by an InventoryButton
     public void Use()
     {
+        GameObject prefab = Resources.Load("Prefabs/Spear") as GameObject; 
+        GameObject spear = Instantiate(prefab, player.transform.position + new Vector3(0.0f, 2.0f, 0.0f), prefab.transform.rotation); //viimast parameetrit mitte näppida, muidu objekt pööratakse vale nurga alla (originaal sprite viltu)
+        //Vector3 difference = mousePointer.transform.position - spear.transform.position;
+        //float rotationZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+        //spear.transform.rotation = Quaternion.Euler(0.0f, 0.0f, rotationZ);
+        //spear.GetComponent<Rigidbody2D>().AddRelativeForce(Vector3.forward * 10);
+
+
+
+        Vector3 mousePos = mousePointer.transform.position;
+        Vector3 spearPos = spear.transform.position;
+        Debug.Log("Mouse position:" + mousePos);
+        Debug.Log("Spear position:" + spearPos);
+        float x = spearPos.x - mousePos.x;
+        float y = spearPos.y - mousePos.y;
+        Debug.Log(x);
+        Debug.Log(y);
+        //spear.GetComponent<Rigidbody2D>().AddForce(new Vector2(x, y)*50);
     }
 
     public void PickUp()
     {
+        //GameObject spear = Instantiate(Resources.Load("Prefabs/Spear") as GameObject, player.transform, true); //intended to display the spear above the player when it is primed, but currently the physics will conflict with the implementation
         pickedUp = true;
     }
 
