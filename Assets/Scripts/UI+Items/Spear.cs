b45b92c47d32a3daa28f1d3f2076c7695090a813
OliVinDiisel <oliver.vinkel@gmail.com>
2018-11-23 13:24:02 +0200

    Enemies now die when hit by the spear. Spear colliders are a bit wonky and don't always pass through the enemy smoothly

--- Assets/Scripts/UI+Items/Spear.cs
+++ Assets/Scripts/UI+Items/Spear.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Text.RegularExpressions;
 
 public class Spear : MonoBehaviour {
     Rigidbody2D rgbd2D;
@@ -29,21 +30,47 @@ public class Spear : MonoBehaviour {
         PolygonCollider2D Collider = (PolygonCollider2D) collision.otherCollider; //Võib lisada try catch ploki
         int PointCount = Collider.points.Length;
 
-        if (collision.gameObject.tag == "Enemy") //et ei takistaks vastaste liikumist
-        {
-            if (!stuck)
-            {
-                //TODO: murderize enemy
-            }
-
-            return;
-        }
         if (PointCount == 3) // TABATI ODA TIPPU
         {
-            replaceSpear();
+            if (collision.gameObject.tag == "Enemy")
+            {
+                Quaternion deadRotation = Quaternion.Euler(0, 0, 90);
+                Vector3 deadShift = new Vector3(0, 0.3f, 0);
+
+                // To deal with the (number) stuff when cloning prefabs
+                Regex dinostroller = new Regex("DinoStroller.*");
+                Regex dinorunner = new Regex("DinoRunner.*");
+                Regex dinostatue = new Regex("DinoStatue.*");
+                Regex dinowalker = new Regex("DinoWalker.*");
+
+                //Tuvastada vastase tüüp
+                if (dinostroller.IsMatch(collision.gameObject.name))
+                {
+                    GameObject.Instantiate(Resources.Load("Prefabs/Enemies/DeadEnemies/DeadDinoStroller"), collision.gameObject.transform.position - deadShift, deadRotation);
+                }
+                else if (dinorunner.IsMatch(collision.gameObject.name))
+                {
+                    GameObject.Instantiate(Resources.Load("Prefabs/Enemies/DeadEnemies/DeadDinoRunner"), collision.gameObject.transform.position - deadShift, deadRotation);
+                }
+                else if (dinostatue.IsMatch(collision.gameObject.name))
+                {
+                    GameObject.Instantiate(Resources.Load("Prefabs/Enemies/DeadEnemies/DeadDinoStatue"), collision.gameObject.transform.position - deadShift, deadRotation);
+                }
+                else if (dinowalker.IsMatch(collision.gameObject.name))
+                {
+                    GameObject.Instantiate(Resources.Load("Prefabs/Enemies/DeadEnemies/DeadDinoWalker"), collision.gameObject.transform.position - deadShift, deadRotation);
+                }
+
+                GameObject.Destroy(collision.gameObject);
+            }
+            else
+            {
+                replaceSpear();
+            }
         }
         else // TABATI ODA TÜVE VÕI PEA LAIEMAT OSA (ST KÜLJE POOLT)
         {
+            if (collision.gameObject.tag == "Enemy") return;
             Invoke("replaceSpear", 3f); // TODO: hetkel vahetatakse füüsiline oda UI oma vastu välja 3 sekundit hiljem, aga see peaks toimuma siis kui oda seisma jäänud.
         }
     }
