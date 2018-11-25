1518d1da3c21830d45c240cb4679bb59b0a358c7
KaarelRyysak <kaarelgames@ryysak.com>
2018-11-25 19:33:03 +0200

    bug fix

--- Assets/Scripts/Movement/CharacterController2.cs
+++ Assets/Scripts/Movement/CharacterController2.cs
@@ -228,6 +228,7 @@ public class CharacterController2 : MonoBehaviour {
         TeleportTime = Time.time;
         teleporting = true;
         grounded = false;
+        anim.SetBool("IsJumping", true);
     }
 
     ////private void OnCollisionEnter2D(Collision2D collision) { if (collision.gameObject.tag == "UISpear" || collision.gameObject.tag == "WalkableTerrain") grounded = true; }
