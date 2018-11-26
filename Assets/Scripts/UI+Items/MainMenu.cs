6e7f6d9bdc0285e29b1aac90b7428dab58ded907
OliVinDiisel <oliver.vinkel@gmail.com>
2018-11-26 11:49:55 +0200

    Added a level selection window

--- Assets/Scripts/UI+Items/MainMenu.cs
+++ Assets/Scripts/UI+Items/MainMenu.cs
@@ -2,6 +2,16 @@
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour {
+    GameObject buttons;
+    GameObject levels;
+    private void Awake()
+    {
+        buttons = GameObject.Find("Buttons");
+        levels = GameObject.Find("Levels");
+
+        levels.SetActive(false);
+    }
+
     public void StartGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
@@ -9,6 +19,18 @@ public class MainMenu : MonoBehaviour {
 
     public void LevelSelect()
     {
-        SceneManager.LoadScene(8);
+        buttons.SetActive(false);
+        levels.SetActive(true);
+    }
+
+    public void Back()
+    {
+        levels.SetActive(false);
+        buttons.SetActive(true);
+    }
+
+    public void StartLevel(int level)
+    {
+        SceneManager.LoadScene(level);
     }
 }
