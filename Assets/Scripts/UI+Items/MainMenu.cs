a75af9c4d8053339db14322611eada8a7322c640
KaarelRyysak <kaarelgames@ryysak.com>
2019-01-17 20:03:55 +0200

    audio tweaks

--- Assets/Scripts/UI+Items/MainMenu.cs
+++ Assets/Scripts/UI+Items/MainMenu.cs
@@ -56,6 +56,8 @@ public class MainMenu : MonoBehaviour {
     {
         buttons.SetActive(false);
         levels.SetActive(true);
+
+        AudioPlayer.instance.clickGroup.Play();
     }
 
     public void Back()
@@ -63,6 +65,8 @@ public class MainMenu : MonoBehaviour {
         levels.SetActive(false);
         buttons.SetActive(true);
         instructions.SetActive(false);
+
+        AudioPlayer.instance.clickGroup.Play();
     }
 
     public void Instructions()
